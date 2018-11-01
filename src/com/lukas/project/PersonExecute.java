ce17f55d8fb2d8237759d232cbf28a537ad088ee
LukasKodym <lukasz.kodym@gmail.com>
2018-11-01 12:54:08 +0100

    inserting sort method to main class

--- src/com/lukas/project/PersonExecute.java
+++ src/com/lukas/project/PersonExecute.java
@@ -2,10 +2,16 @@ package com.lukas.project;
 
 import java.util.*;
 
-public class PersonExecute {
+
+
+public class PersonExecute extends SortingMethods{
 
     public static void main(String[] args) {
         Person[] person = Person.exampleArray();
+        inSort(person);
         System.out.println(Arrays.toString(person));
     }
+
+
+
 }
