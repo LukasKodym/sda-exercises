e75afb26575a861fd33409bf49e21e75656656bc
LukasKodym <lukasz.kodym@gmail.com>
2018-11-03 13:58:42 +0100

    actualisation

---
+++ src/com/lukas/data_scructures/Main.java
@@ -0,0 +1,13 @@
+package com.lukas.data_scructures;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        QueueArray q = new QueueArray();
+        q.push(2);
+
+
+        System.out.println();
+    }
+}
--- src/com/lukas/data_scructures/QueueArray.java
+++ src/com/lukas/data_scructures/QueueArray.java
@@ -2,6 +2,20 @@ package com.lukas.data_scructures;
 
 public class QueueArray {
 
+    private int[] array = new int[10];
+    private int size = 0;
 
+    public void push(int i) {
+
+        array[size++] = i;
+
+    }
+
+    public int pop(){
+        return 0;
+    }
 
 }
+
+
+
