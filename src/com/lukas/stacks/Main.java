cd7f5594731e0313920327194a51b443ec8a998b
LukasKodym <lukasz.kodym@gmail.com>
2018-11-04 15:00:17 +0100

    refresh

--- src/com/lukas/stacks/Main.java
+++ src/com/lukas/stacks/Main.java
@@ -8,14 +8,20 @@ public class Main {
         String result = "";
         Stack<Integer> stack = new Stack<>();
         do {
-
-
-
+            stack.push(number % base);
+            number = number / base;
         } while (number > 0);
         while (!stack.empty()) {
-
+            result += stack.peek();
+            stack.pop();
         }
         return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(convertBase(12,2));
+        System.out.println(convertBase(12,8));
+        System.out.println(convertBase(0b1011,2));
+        System.out.println(convertBase(066,8));
     }
 
 }
