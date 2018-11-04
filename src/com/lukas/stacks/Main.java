7e7f8b7735d61b71bf8ebfe497481a59257e0cc5
LukasKodym <lukasz.kodym@gmail.com>
2018-11-04 14:44:33 +0100

    refresh

--- src/com/lukas/stacks/Main.java
+++ src/com/lukas/stacks/Main.java
@@ -1,4 +1,21 @@
 package com.lukas.stacks;
 
+import java.util.Stack;
+
 public class Main {
+
+    public static String convertBase(int number, int base) {
+        String result = "";
+        Stack<Integer> stack = new Stack<>();
+        do {
+
+
+
+        } while (number > 0);
+        while (!stack.empty()) {
+
+        }
+        return result;
+    }
+
 }
