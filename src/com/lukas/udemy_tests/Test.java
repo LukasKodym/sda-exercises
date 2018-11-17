2acf4194e2b06eb8c8f6ea4464bb5c0ecaf9e028
LukasKodym <lukasz.kodym@gmail.com>
2018-11-17 11:47:16 +0100

    commit

---
+++ src/com/lukas/udemy_tests/Test.java
@@ -0,0 +1,33 @@
+package com.lukas.udemy_tests;
+
+public class Test {
+
+    private int z;
+    private int x;
+
+    public int getZ() {
+        return z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Test(int z, int x) {
+        this.z = z;
+        this.x = x;
+    }
+
+    public Test() {
+    }
+
+}
